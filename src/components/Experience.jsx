15ca5d907ce1181e54d2bde2c69b0acfe318115c
Gaurav Wakhare <gwakhare2418@gmail.com>
2025-03-31 11:24:00 +0530

    UI change

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,26 +4,32 @@ const Experience = () => {
     return(
 
 
+        // flex flex-col justify-center items-center w-full w-full h-screen
+<div name='experience' className=" bg-[#0a192f] " >
+    <div className="w-full">
+        <p className='bg-[#0a192f] text-white  text-4xl font-bold  border-b-4 border-pink-600 '>
+            Work Experience
+        </p>
+    </div>
 
-<div name='experience' className="w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center w-full" >
-<p className='bg-[#0a192f] text-white  text-4xl font-bold  border-b-4 border-pink-600 '>Work Experience</p>
 <section class=" dark:bg-gray-900">
     <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-        <div className="bg-gray-300 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+        <div className="bg-gray-300 mb-[100px] dark:bg-[#EFF3EA] border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
             {/* <a href="#" class="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2">
                 <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
                     <path d="M11 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm8.585 1.189a.994.994 0 0 0-.9-.138l-2.965.983a1 1 0 0 0-.685.949v8a1 1 0 0 0 .675.946l2.965 1.02a1.013 1.013 0 0 0 1.032-.242A1 1 0 0 0 20 12V2a1 1 0 0 0-.415-.811Z"/>
                 </svg>
                 Tutorial
             </a> */}
-            <div class='flex justify-between'>
+
+            <div class='sm:flex sm:justify-between'>
             <h2 class="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">Next Step Technologies.</h2>
             <h3 class="mt-5 font-semibold dark:text-white">December 2024 - Current</h3>
             </div>
             <h2 class="text-gray-600 font-extrabold m-3 dark:text-white">Junior Software Engineer</h2>
             
                 
-                <ul class="max-w-screen-xl ml-6 mr-[150px] space-y-1 text-gray-600 list-disc list-inside dark:text-gray-400">
+                <ul class="max-w-screen-xl sm:ml-6 sm:mr-[120px] space-y-1 text-gray-600 list-disc list-inside dark:text-gray-400">
                     <li>
                     Developed a fully automated testing system using LabVIEW to monitor and control the performance of electronic
                     devices under various conditions. This system reduced testing time by 30% and improved accuracy.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,13 @@ const Navbar = () => {
         <li className='py-6 text-4xl'>
           {' '}
           <Link onClick={handleClick} to='work' smooth={true} duration={500}>
-            Work
+            Projects
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='experience' smooth={true} duration={500}>
+            Experience
           </Link>
         </li>
         <li className='py-6 text-4xl'>
